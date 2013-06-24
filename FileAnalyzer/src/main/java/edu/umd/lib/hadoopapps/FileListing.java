a0601455bc66103c1d25de9f396f985cd2a990a0
Mohamed Mohideen Abdul Rasheed <mohideen@umd.edu>
2013-06-24 17:30:34 -0400

    Generate MD5hash with specified num. of map tasks.

--- FileAnalyzer/src/main/java/edu/umd/lib/hadoopapps/FileListing.java
+++ FileAnalyzer/src/main/java/edu/umd/lib/hadoopapps/FileListing.java
@@ -15,12 +15,54 @@ import org.apache.hadoop.util.ToolRunner;
 
 public class FileListing extends Configured implements Tool {
   
-  private FileListing() {
-  }
-  
   private void usage() {
     System.err.println("Usage: listpaths sourceDir outputPath ");
     ToolRunner.printGenericCommandUsage(System.out);
+  }
+  
+  public long createListing(Path sourcePath, Path outputPath)  throws Exception {
+    Configuration conf = new Configuration();
+    FileSystem hdfs = FileSystem.get(conf);
+    long numPaths = 0;
+    SequenceFile.Writer.Option pathOpt = SequenceFile.Writer.file(outputPath);
+    SequenceFile.Writer.Option keyClassOpt = SequenceFile.Writer.keyClass(Text.class);
+    SequenceFile.Writer.Option valClassOpt = SequenceFile.Writer.valueClass(NullWritable.class);
+    SequenceFile.Writer.Option optCom = SequenceFile.Writer.compression(SequenceFile.CompressionType.NONE);
+    
+    if(hdfs.exists(sourcePath)) {
+      hdfs.create(outputPath, true);
+      SequenceFile.Writer outputList = SequenceFile.createWriter(conf, pathOpt, keyClassOpt, 
+          valClassOpt, optCom);
+      Text pathString = new Text();
+      NullWritable nWritable = NullWritable.get();
+      if(hdfs.isDirectory(sourcePath)) {
+        RemoteIterator<LocatedFileStatus> pathIterator = (RemoteIterator<LocatedFileStatus>)hdfs.listFiles(sourcePath, true);
+        FileStatus currentFile;
+        
+        while(pathIterator.hasNext()) {
+          currentFile = (FileStatus)pathIterator.next();
+          pathString.set(currentFile.getPath().toString());
+          //System.out.println(currentFile.getPath().toString());
+          outputList.append(pathString, nWritable);
+          numPaths++;
+        }
+        outputList.close();
+        
+        /*
+        SequenceFile.Reader.Option pathOptR = SequenceFile.Reader.file(outputPath);
+        SequenceFile.Reader sReader = new SequenceFile.Reader(getConf(), pathOptR);
+        while(sReader.next(pathString)) {
+          System.out.println(pathString.toString());
+        }*/
+        
+      } else {
+        System.out.println("Source path is not a directory!");    
+      }
+
+    } else {
+      System.out.println("Invalid source path!");     
+    }
+    return numPaths;
   }
   
   /**
@@ -36,43 +78,7 @@ public class FileListing extends Configured implements Tool {
     
     Path sourcePath = new Path(args[0]);
     Path outputPath = new Path(args[1]);
-    FileSystem hdfs = FileSystem.get(new Configuration());
-    SequenceFile.Writer.Option pathOpt = SequenceFile.Writer.file(outputPath);
-    SequenceFile.Writer.Option keyClassOpt = SequenceFile.Writer.keyClass(Text.class);
-    SequenceFile.Writer.Option valClassOpt = SequenceFile.Writer.valueClass(NullWritable.class);
-    SequenceFile.Writer.Option optCom = SequenceFile.Writer.compression(SequenceFile.CompressionType.NONE);
-    
-    if(hdfs.exists(sourcePath)) {
-      hdfs.create(outputPath, true);
-      SequenceFile.Writer outputList = SequenceFile.createWriter(getConf(), pathOpt, keyClassOpt, 
-          valClassOpt, optCom);
-      Text pathString = new Text();
-      NullWritable nWritable = NullWritable.get();
-      if(hdfs.isDirectory(sourcePath)) {
-        RemoteIterator<LocatedFileStatus> pathIterator = (RemoteIterator<LocatedFileStatus>)hdfs.listFiles(sourcePath, true);
-        FileStatus currentFile;
-        
-        while(pathIterator.hasNext()) {
-          currentFile = (FileStatus)pathIterator.next();
-          pathString.set(currentFile.getPath().toString());
-          //System.out.println(currentFile.getPath().toString());
-          outputList.append(pathString, nWritable);
-        }
-        outputList.close();
-        
-        SequenceFile.Reader.Option pathOptR = SequenceFile.Reader.file(outputPath);
-        SequenceFile.Reader sReader = new SequenceFile.Reader(getConf(), pathOptR);
-        while(sReader.next(pathString)) {
-          System.out.println(pathString.toString());
-        }
-        
-      } else {
-        System.out.println("Source path is not a directory!");    
-      }
-
-    } else {
-      System.out.println("Invalid source path!");     
-    }
+    createListing(sourcePath, outputPath);
     
     return 0;
   }
--- FileAnalyzer/src/main/java/edu/umd/lib/hadoopapps/MD5Batch.java
+++ FileAnalyzer/src/main/java/edu/umd/lib/hadoopapps/MD5Batch.java
@@ -9,6 +9,7 @@ import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.MD5Hash;
 import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.SequenceFile;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.JobContext;
@@ -49,6 +50,64 @@ public class MD5Batch extends Configured implements Tool {
     System.err.println("Usage: MD5Batch inputFile outputFile numMaps ");
     ToolRunner.printGenericCommandUsage(System.out);
   }
+  
+  private Path createMapInputs(Path sourcePath, int numMaps) throws Exception{
+    FileSystem hdfs = FileSystem.get(getConf());
+    String pathPrefix = "/tmp/md5batch/" + System.currentTimeMillis() + sourcePath.getName();
+    Path listPath = new Path(pathPrefix + "-list.seq");
+    FileListing fileListing = new FileListing();
+    Path mapInputDir;
+    try {
+      long numPaths = fileListing.createListing(sourcePath, listPath);
+      mapInputDir = new Path(pathPrefix + "-inputdir");
+      hdfs.mkdirs(mapInputDir);
+      SequenceFile.Reader.Option pathOptR = SequenceFile.Reader.file(listPath);
+      SequenceFile.Reader sReader = new SequenceFile.Reader(getConf(), pathOptR);
+      
+      SequenceFile.Writer.Option pathOpt;
+      SequenceFile.Writer.Option keyClassOpt = SequenceFile.Writer.keyClass(Text.class);
+      SequenceFile.Writer.Option valClassOpt = SequenceFile.Writer.valueClass(NullWritable.class);
+      SequenceFile.Writer.Option optCom = SequenceFile.Writer.compression(SequenceFile.CompressionType.NONE);
+      Text pathString = new Text();
+      NullWritable nWritable = NullWritable.get();
+      Path tmpInputFilePath;
+      SequenceFile.Writer seqFileWriter;
+      if(numMaps > numPaths) {
+        for(int count=0; count<numPaths; count++) {
+          tmpInputFilePath = new Path(mapInputDir.toString() + "/part-" + count);
+          pathOpt = SequenceFile.Writer.file(tmpInputFilePath);
+          seqFileWriter = SequenceFile.createWriter(getConf(), pathOpt, keyClassOpt, 
+              valClassOpt, optCom);
+          sReader.next(pathString);
+          seqFileWriter.append(pathString, nWritable);
+        }
+      } else {
+        long addedPaths = 0;
+        for(int count=1; count<=numMaps; count++) {
+          tmpInputFilePath = new Path(mapInputDir.toString() + "/part-" + count);
+          pathOpt = SequenceFile.Writer.file(tmpInputFilePath);
+          seqFileWriter = SequenceFile.createWriter(getConf(), pathOpt, keyClassOpt, 
+              valClassOpt, optCom);
+          while(addedPaths < ((numPaths/numMaps)*count)) {
+            sReader.next(pathString);
+            seqFileWriter.append(pathString, nWritable);
+            addedPaths++;
+          }
+                  
+          if(count == numMaps) {
+            while(sReader.next(pathString)) {
+              seqFileWriter.append(pathString, nWritable);
+            }
+          }
+          seqFileWriter.close();        
+        }
+      }
+    } finally {
+      hdfs.delete(listPath, false);
+    }
+    return mapInputDir;
+    
+  }
 
   public int run(String[] args) throws Exception {
     
@@ -56,20 +115,23 @@ public class MD5Batch extends Configured implements Tool {
       usage();
       System.exit(-1);
     }
-    
+    FileSystem hdfs = FileSystem.get(getConf());
     String inputFile = args[0];
     String outputDir = args[1];
     String numMaps = args[2];
 
     Path inputPath = new Path(inputFile);
     Path outputPath = new Path(outputDir);
+    Path mapInputDir;
+    mapInputDir = createMapInputs(inputPath, Integer.parseInt(numMaps));
     
+      
     Job job = Job.getInstance(getConf());
     job.setJobName("MD5Batch");
-    
+      
     //job.setInputFormatClass(S.class);
     job.setJarByClass(MD5Batch.class);
-    
+     
     job.setMapperClass(MD5BatchMapper.class);
     job.setNumReduceTasks(0);
     job.setInputFormatClass(SequenceFileInputFormat.class);
@@ -77,16 +139,22 @@ public class MD5Batch extends Configured implements Tool {
     job.setMapOutputValueClass(Text.class);
     job.getConfiguration().set(JobContext.MAP_SPECULATIVE, "false");
     job.getConfiguration().set(JobContext.NUM_MAPS, numMaps);
-    
-    SequenceFileInputFormat.addInputPath(job, inputPath);
+    SequenceFileInputFormat.addInputPath(job, mapInputDir);
     FileOutputFormat.setOutputPath(job, outputPath);
     
-    long startTime = System.currentTimeMillis();
-    job.waitForCompletion(true);
-    System.out.println("Completed in " +
-        ((System.currentTimeMillis() - startTime)/1000) + "secs.");
+    try { 
+      long startTime = System.currentTimeMillis();
+      job.waitForCompletion(true);
+      System.out.println("Completed in " +
+          ((System.currentTimeMillis() - startTime)/1000) + "secs.");
+    } finally {
+      if(mapInputDir != null) {
+        hdfs.delete(mapInputDir, true);
+      }
+    }
     
     return 0;
+    
   }
   
   public static void main(String[] args) throws Exception {
